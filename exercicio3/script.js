48514aa5bcdf806d0499a64a8c6ea9465bb2a2bf
dgcezar <diego.cezar13@gmail.com>
2022-09-08 13:12:38 -0300

    att2 - exercício 3 concluído

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,14 @@
 //código a ser reescrito
 
-if(nome === "José"){
+const nome = prompt("Digite seu nome: ");
+const idade = prompt("Digite sua idade: ");
+
+nome === "José" ? alert("Oi, Zé!") : alert("Olá, " + nome);
+
++idade >= 18 ? alert("pode tirar carteira de motorista!") : alert("Ainda não pode tirar carteira de motorista!");
+
+
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
@@ -10,4 +18,4 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
