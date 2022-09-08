c66a625debd3e478615a1f6665c5a5685fa0e34a
dgcezar <diego.cezar13@gmail.com>
2022-09-08 12:55:56 -0300

    Primeiro - primeira parte do exercício 1 feita

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,12 @@
-const num = //seu numero aqui
+const num = prompt("Digite um número: ");
+/*
+if (num % 2 === 0) {
+  alert("O número digitado é par.");
+}
+if (num % 2 === 1) {
+  alert("O número digitado é impar.");
+}*/
 
+if (+num % 2 === 0 || +num % 2 === 1) {
+
+}
